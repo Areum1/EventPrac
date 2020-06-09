9597d49310589ca0eedc424c4151c256eae6a600
Areum1 <s2018s10@e-mirim.hs.kr>
2020-06-09 09:37:08 +0900

    초기 커밋.

---
+++ EventPrac/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventPrac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Text = "아이즈원 최고";
+            btn.Enabled = false;
+            //textBox1.Text += "▒";
+            //label1.Text = label1.Text + "▒";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = "X : " + e.X + " / Y : " + e.Y;
+        }
+    }
+}
